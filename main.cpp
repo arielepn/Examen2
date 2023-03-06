cac673654d6c07ae851b6eba6d333a8b48c5477f
arielepn <127131594+arielepn@users.noreply.github.com>
2023-03-06 12:14:30 -0500

    Add files via upload
    base

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+const int hmCedula =1722966650 ;  
+const string hmNombre_completo ="Harryson Ariel Montesdeoca Rhea" ;  
+
+struct hmCoordenadas{
+    int hmCap;
+    string hmGeo;
+    char hmTipo_Arsenal[10];
+};
+
+
+int main()
+
+{
+    cout << "Hello world!" << endl;
+}
